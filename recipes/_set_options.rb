d63d2c193a07960af921f6d918a86d0631c37df6
Grant Heffernan <heffergm@gmail.com>
2015-06-18 10:24:25 -0400

    lint

--- recipes/_set_options.rb
+++ recipes/_set_options.rb
@@ -11,11 +11,9 @@ end
 
 # if we're told to process geojson, shapes and extracts need to be true
 if node[:mapzen_odes][:process][:osm2pgsql_geojson]  == true
-  node.set[:mapzen_odes][:process][:extracts]         = true
   node.set[:mapzen_odes][:process][:osm2pgsql_shapes] = true
 end
 
 if node[:mapzen_odes][:process][:imposm_geojson]  == true
-  node.set[:mapzen_odes][:process][:extracts]      = true
   node.set[:mapzen_odes][:process][:imposm_shapes] = true
 end
