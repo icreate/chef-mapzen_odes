83cd33eb0a47d50be7ce7d8862690f396254b9ad
Grant Heffernan <heffergm@gmail.com>
2015-06-18 11:47:26 -0400

    fix

--- recipes/extracts.rb
+++ recipes/extracts.rb
@@ -13,7 +13,7 @@ execute 'create extracts' do
       -a #{node[:mapzen_odes][:setup][:scriptsdir]}/extracts.sh \
       --joblog #{node[:mapzen_odes][:setup][:basedir]}/logs/parallel_extracts.log
   EOH
-  only_if { node[:mapzen_odes][:process][:extracts] == true }
+  only_if { node[:mapzen_odes][:process][:pbf_extracts] == true || node[:mapzen_odes][:process][:xml_extracts] == true}
 end
 
 execute 'fix osmconvert perms' do
@@ -21,5 +21,5 @@ execute 'fix osmconvert perms' do
   user      node[:mapzen_odes][:user][:id]
   cwd       node[:mapzen_odes][:setup][:basedir]
   command   'chmod 644 ex/*'
-  only_if   { node[:mapzen_odes][:process][:extracts] == true }
+  only_if { node[:mapzen_odes][:process][:pbf_extracts] == true || node[:mapzen_odes][:process][:xml_extracts] == true}
 end
