d3093c807f06cc24e74ad8a232e9c26f62b61420
Grant Heffernan <heffergm@gmail.com>
2015-06-18 08:13:41 -0400

    fix cron

--- recipes/planet.rb
+++ recipes/planet.rb
@@ -40,7 +40,7 @@ end
 cron 'update planet' do
   command "#{node[:mapzen_odes][:setup][:scriptsdir]}/update_planet.sh >#{node[:mapzen_odes][:setup][:basedir]}/logs/update_planet.log 2>&1"
   user    node[:mapzen_odes][:user][:id]
-  home    node[:mapzen_odes][:setup][:basedir]
+  home    "#{node[:mapzen_odes][:setup][:basedir]}/data"
   hour    '5'
   minute  '*'
   weekday '*'
