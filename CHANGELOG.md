efa6ca310f04c6829ab686751bff81746e7309ec
Grant Heffernan <heffergm@gmail.com>
2015-06-19 06:02:05 -0400

    0.6.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 mapzen_odes changelog
 =====================
 
+0.6.3
+-----
+* use 4236 everywhere
+
 0.6.2
 -----
 * use 4326 for imposm
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'grant@mapzen.com'
 license          'GPL v3'
 description      'Installs/Configures odes'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.6.2'
+version          '0.6.3'
 
 recipe 'mapzen_odes', 'Builds metro extracts ode service'
 
