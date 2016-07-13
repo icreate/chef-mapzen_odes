efd62dde259a19d3c9515e259a27301c465782b2
Grant Heffernan <heffergm@gmail.com>
2016-07-13 12:57:17 -0400

    0.11.0

--- recipes/coastlines.rb
+++ recipes/coastlines.rb
@@ -3,27 +3,43 @@
 # Recipe:: coastlines
 #
 
-execute 'wget water polygons' do
-  user    node[:mapzen_odes][:user][:id]
-  cwd     "#{node[:mapzen_odes][:setup][:basedir]}/data"
-  command <<-EOH
-    wget --quiet -O #{node[:mapzen_odes][:coastlines][:water_polygons][:file]} \
-      #{node[:mapzen_odes][:coastlines][:water_polygons][:url]} &&
-      unzip #{node[:mapzen_odes][:coastlines][:water_polygons][:file]}
-  EOH
-  not_if  { ::File.exist?("#{node[:mapzen_odes][:setup][:basedir]}/data/#{node[:mapzen_odes][:coastlines][:water_polygons][:file]}") }
+remote_file "#{node[:mapzen_odes][:setup][:basedir]}/data/#{node[:mapzen_odes][:coastlines][:water_polygons][:file]}" do
+  owner       node[:mapzen_odes][:user][:id]
+  mode        0644
+  retries     2
+  retry_delay 60
+  backup      false
+  source      node[:mapzen_odes][:coastlines][:water_polygons][:url]
+  notifies    :run, 'execute[unzip water polygons]', :immediately
 end
 
-execute 'wget land polygons' do
+execute 'unzip water polygons' do
+  action  :nothing
   user    node[:mapzen_odes][:user][:id]
   cwd     "#{node[:mapzen_odes][:setup][:basedir]}/data"
-  command <<-EOH
-    wget --quiet -O #{node[:mapzen_odes][:coastlines][:land_polygons][:file]} \
-      #{node[:mapzen_odes][:coastlines][:land_polygons][:url]} &&
-      unzip #{node[:mapzen_odes][:coastlines][:land_polygons][:file]}
-  EOH
-  not_if  { ::File.exist?("#{node[:mapzen_odes][:setup][:basedir]}/data/#{node[:mapzen_odes][:coastlines][:land_polygons][:file]}") }
+  command <<-EOF
+    unzip -o #{node[:mapzen_odes][:coastlines][:water_polygons][:file]}
+  EOF
+end 
+
+remote_file "#{node[:mapzen_odes][:setup][:basedir]}/data/#{node[:mapzen_odes][:coastlines][:land_polygons][:file]}" do
+  owner       node[:mapzen_odes][:user][:id]
+  mode        0644
+  retries     2
+  retry_delay 60
+  backup      false
+  source      node[:mapzen_odes][:coastlines][:water_polygons][:url]
+  notifies    :run, 'execute[unzip land polygons]', :immediately
 end
+
+execute 'unzip land polygons' do
+  action  :nothing
+  user    node[:mapzen_odes][:user][:id]
+  cwd     "#{node[:mapzen_odes][:setup][:basedir]}/data"
+  command <<-EOF
+    unzip -o #{node[:mapzen_odes][:coastlines][:land_polygons][:file]}
+  EOF
+end 
 
 execute 'generate coastlines' do
   user    node[:mapzen_odes][:user][:id]
